6fcdefdfd3743e2067ebd1bcefcc07fd638e465e
mazzocchifrancesco <123@email.it>
2024-01-26 16:02:34 +0100

    fix scss

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -23,4 +23,17 @@
         </div>
     </div>
 </div>
+
+{{-- Bluebar --}}
+    
+<div id="bluebar">
+    <div class="container d-flex justify-content-between">
+        @foreach ($bluebarLinks as $item)
+        <div class="d-flex align-items-center linkCard">
+            <img class="blueIcon" src='/Assets/images/{{$item['img']}}' alt="">
+            <a href=" #" class="text-white fw-semibold text-uppercase ms-3 text-decoration-none">{{ $item['title'] }}</a>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,17 +1,6 @@
 <footer>
 
-    {{-- Bluebar --}}
     
-        <div id="bluebar">
-            <div class="container d-flex justify-content-between">
-                @foreach ($bluebarLinks as $item)
-                <div class="d-flex align-items-center linkCard">
-                    <img class="blueIcon" src='/Assets/images/{{$item['img']}}' alt="">
-                    <a href=" #" class="text-white fw-semibold text-uppercase ms-3 text-decoration-none">{{ $item['title'] }}</a>
-                </div>
-                @endforeach
-            </div>
-        </div>
     
         {{-- MAIN footer --}}
     
