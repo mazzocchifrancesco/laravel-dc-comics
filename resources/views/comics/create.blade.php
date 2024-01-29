b4d669fab0150e289285fb4287ce5b5b5222e990
mazzocchifrancesco <123@email.it>
2024-01-29 16:33:07 +0100

    add old value

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -19,49 +19,49 @@
                 @csrf
                 <div class="mb-3">
                     <label for="title" class="form-label">title</label>
-                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" >
+                    <input type="text" class="form-control @error('title') is-invalid @enderror" id="title" name="title" value="{{old("title")}}" >
                     @error('title')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="description" class="form-label">description</label>
-                    <input type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" >
+                    <input type="text" class="form-control @error('description') is-invalid @enderror" id="description" name="description" value="{{old("description")}}">
                     @error('description')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="thumb" class="form-label">thumb</label>
-                    <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb">
+                    <input type="text" class="form-control @error('thumb') is-invalid @enderror" id="thumb" name="thumb" value="{{old("thumb")}}">
                     @error('thumb')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="price" class="form-label">price</label>
-                    <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" >
+                    <input type="number" step="0.01" class="form-control @error('price') is-invalid @enderror" id="price" name="price" value="{{old("price")}}">
                     @error('price')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="series" class="form-label">series</label>
-                    <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" >
+                    <input type="text" class="form-control @error('series') is-invalid @enderror" id="series" name="series" value="{{old("series")}}">
                     @error('series')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="sale_date" class="form-label">sale_date</label>
-                    <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" >
+                    <input type="date" class="form-control @error('sale_date') is-invalid @enderror" id="sale_date" name="sale_date" value="{{old("sale_date")}}">
                     @error('sale_date')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
                 </div>
                 <div class="mb-3">
                     <label for="type" class="form-label">type</label>
-                    <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" >
+                    <input type="text" class="form-control @error('type') is-invalid @enderror" id="type" name="type" value="{{old("type")}}">
                     @error('type')
                         <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
